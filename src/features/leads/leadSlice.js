2dc4e7911f340e859231994ffbb2b8cd1a240a90
felpsg <felipetec.sso@gmail.com>
2024-01-07 16:50:20 -0300

    refactor compon notification

--- src/features/leads/leadSlice.js
+++ src/features/leads/leadSlice.js
@@ -1,47 +1,55 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-
-
-export const getLeadsContent = createAsyncThunk('/leads/content', async () => {
-	const response = await axios.get('/api/users?page=2', {})
-	return response.data;
-})
+export const getLeadsContent = createAsyncThunk(
+  "leads/getContent",
+  async () => {
+    try {
+      const response = await axios.get("https://reqres.in/api/users?page=2");
+      const leadsWithAdditionalInfo = response.data.data.map((user) => ({
+        name: user.first_name,
+        surname: user.last_name,
+        email: user.email,
+        cpf: "",
+        rg: "",
+        address: "",
+      }));
+      return { data: leadsWithAdditionalInfo };
+    } catch (error) {
+      console.error("Erro ao buscar leads:", error);
+      return { data: [] };
+    }
+  },
+);
 
 export const leadsSlice = createSlice({
-    name: 'leads',
-    initialState: {
-        isLoading: false,
-        leads : []
+  name: "leads",
+  initialState: {
+    isLoading: false,
+    leads: [],
+  },
+  reducers: {
+    addNewLead: (state, action) => {
+      state.leads.push(action.payload);
     },
-    reducers: {
-
-
-        addNewLead: (state, action) => {
-            let {newLeadObj} = action.payload
-            state.leads = [...state.leads, newLeadObj]
-        },
-
-        deleteLead: (state, action) => {
-            let {index} = action.payload
-            state.leads.splice(index, 1)
-        }
+    deleteLead: (state, action) => {
+      state.leads.splice(action.payload.index, 1);
     },
+  },
+  extraReducers: {
+    [getLeadsContent.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getLeadsContent.fulfilled]: (state, action) => {
+      state.leads = action.payload.data;
+      state.isLoading = false;
+    },
+    [getLeadsContent.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
+});
 
-    extraReducers: {
-		[getLeadsContent.pending]: state => {
-			state.isLoading = true
-		},
-		[getLeadsContent.fulfilled]: (state, action) => {
-			state.leads = action.payload.data
-			state.isLoading = false
-		},
-		[getLeadsContent.rejected]: state => {
-			state.isLoading = false
-		},
-    }
-})
+export const { addNewLead, deleteLead } = leadsSlice.actions;
 
-export const { addNewLead, deleteLead } = leadsSlice.actions
-
-export default leadsSlice.reducer
+export default leadsSlice.reducer;
