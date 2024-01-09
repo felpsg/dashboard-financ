3a7cd505a5cbcb86e4bd8b38ce80bd055bc69c0e
felpsg <felipetec.sso@gmail.com>
2024-01-09 00:33:11 -0300

    add filter in cadastro

--- src/features/leads/index.js
+++ src/features/leads/index.js
@@ -1,5 +1,8 @@
+import FunnelIcon from "@heroicons/react/24/outline/FunnelIcon";
+// Importações necessárias
 import EditIcon from "@heroicons/react/24/outline/PencilIcon";
 import TrashIcon from "@heroicons/react/24/outline/TrashIcon";
+import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -27,7 +30,13 @@ function ConfirmDeleteModal({ isOpen, onClose, onConfirm, leadId }) {
   );
 }
 
-const TopSideButtons = ({ applySearch }) => {
+// Componente TopSideButtons
+const TopSideButtons = ({
+  applySearch,
+  applyFilter,
+  removeFilter,
+  filterParam,
+}) => {
   const dispatch = useDispatch();
   const [searchText, setSearchText] = useState("");
 
@@ -41,6 +50,14 @@ const TopSideButtons = ({ applySearch }) => {
     );
   };
 
+  const locationFilters = [
+    "Cidade1",
+    "Cidade2",
+    "Cidade3",
+    "Cidade4",
+    "Cidade5",
+  ]; // Substitua com suas opções de filtro
+
   return (
     <div className="inline-block float-right">
       <SearchBar
@@ -49,21 +66,52 @@ const TopSideButtons = ({ applySearch }) => {
         setSearchText={setSearchText}
       />
       <button
-        className="btn px-6 btn-sm normal-case btn-primary"
+        className="btn px-6 btn-sm normal-case btn-primary mr-9"
         onClick={openAddNewLeadModal}
       >
         Adicionar Novo Cliente
       </button>
+      {filterParam && (
+        <button
+          onClick={removeFilter}
+          className="btn btn-xs mr-2 btn-active btn-ghost normal-case"
+        >
+          {filterParam}
+          <XMarkIcon className="w-4 ml-2" />
+        </button>
+      )}
+      <div className="dropdown dropdown-bottom dropdown-end">
+        <label tabIndex={0} className="btn btn-sm btn-outline">
+          <FunnelIcon className="w-5 mr-2" />
+          Filter
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu p-2 text-sm shadow bg-base-100 rounded-box w-52"
+        >
+          {locationFilters.map((l, k) => (
+            <li key={k}>
+              <a onClick={() => applyFilter(l)}>{l}</a>
+            </li>
+          ))}
+          <div className="divider mt-0 mb-0"></div>
+          <li>
+            <a onClick={removeFilter}>Remove Filter</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+// Componente Leads
 function Leads() {
   const { leads } = useSelector((state) => state.lead);
   const dispatch = useDispatch();
   const [filteredLeads, setFilteredLeads] = useState([]);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [selectedLeadId, setSelectedLeadId] = useState(null);
+  const [filterParam, setFilterParam] = useState("");
 
   useEffect(() => {
     dispatch(getLeadsContent());
@@ -74,17 +122,28 @@ function Leads() {
   }, [leads]);
 
   const applySearch = (searchValue) => {
-    if (!searchValue) {
-      setFilteredLeads(leads);
-      return;
+    let newFiltered = leads;
+    if (searchValue) {
+      newFiltered = newFiltered.filter(
+        (lead) =>
+          lead.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+          lead.surname.toLowerCase().includes(searchValue.toLowerCase()),
+      );
     }
-    const filtered = leads.filter(
-      (lead) =>
-        lead.name.toLowerCase().includes(searchValue.toLowerCase()) ||
-        lead.surname.toLowerCase().includes(searchValue.toLowerCase()),
-      // Adicione outras condições de pesquisa conforme necessário
-    );
-    setFilteredLeads(filtered);
+    if (filterParam) {
+      newFiltered = newFiltered.filter((lead) => lead.location === filterParam);
+    }
+    setFilteredLeads(newFiltered);
+  };
+
+  const applyFilter = (filter) => {
+    setFilterParam(filter);
+    applySearch(""); // Reinicia a pesquisa quando um filtro é aplicado
+  };
+
+  const removeFilter = () => {
+    setFilterParam("");
+    applySearch(""); // Atualiza a lista quando o filtro é removido
   };
 
   const openDeleteModal = (leadId) => {
@@ -119,7 +178,14 @@ function Leads() {
       <TitleCard
         title="Clientes Atuais"
         topMargin="mt-2"
-        TopSideButtons={<TopSideButtons applySearch={applySearch} />}
+        TopSideButtons={
+          <TopSideButtons
+            applySearch={applySearch}
+            applyFilter={applyFilter}
+            removeFilter={removeFilter}
+            filterParam={filterParam}
+          />
+        }
       >
         <div className="overflow-x-auto w-full">
           <table className="table w-full">
